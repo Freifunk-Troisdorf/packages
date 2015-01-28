0c249e15eac93844cb05156a6731b62abc7abbb7
stebifan <stefan@tro-mail.de>
2015-01-29 00:29:29 +0100

    Update netwatch.sh

--- netwatch/files/lib/tro/netwatch/netwatch.sh
+++ netwatch/files/lib/tro/netwatch/netwatch.sh
@@ -1,6 +1,6 @@
 if ping -c 1 -w 5 8.8.8.8 > '/dev/null' 2>&1
 		then exit
-    elif ping -c 1 -w 10.188.0.241 > '/dev/null' 2>&1
+    elif ping -c 1 -w 5 10.188.0.241 > '/dev/null' 2>&1
         then exit
     elif ping6 -c 1 -w 5 2001:4860:4860::8888 > '/dev/null' 2>&1
     	then exit
