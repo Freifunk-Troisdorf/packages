fb9abfb2220685da5c0e6efd63c18ea8d9c4a771
xeaon <kemal@soyguder.de>
2015-01-28 15:59:20 +0100

    Update netwatch.sh

--- netwatch/files/lib/tro/netwatch/netwatch.sh
+++ netwatch/files/lib/tro/netwatch/netwatch.sh
@@ -3,12 +3,12 @@
 # This script forces a reboot if ping to Google DNS Servers and a supernode fails
 
 # ping google
-internet = $(ping -c 1 8.8.8.8)
+internet=$(ping -c 1 8.8.8.8)
 
 # if ping fails
 if [ $internet -ne "0" ]; then
         # ping supernode to be sure, that it's only the internet connection
-        $supernode = $(ping -c 1 10.188.0.241)
+        $supernode=$(ping -c 1 10.188.0.241)
         # if ping to supernode fails
         if [ $supernode -ne "0" ]; then
                 reboot
