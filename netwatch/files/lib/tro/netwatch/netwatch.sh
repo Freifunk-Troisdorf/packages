cc693fb02cd75c625e302d1432f7001f1cceaf3c
stebifan <stefan@tro-mail.de>
2015-01-28 18:10:08 +0100

    Update netwatch.sh

--- netwatch/files/lib/tro/netwatch/netwatch.sh
+++ netwatch/files/lib/tro/netwatch/netwatch.sh
@@ -1,7 +1,7 @@
-if ping -c 1 -w 5 8.8.8.9 > '/dev/null' 2>&1
-then echo 'Online'
-    elif ping -c 1 -w 5 8.8.8.10 > '/dev/null' 2>&1
-        then echo "online 2"
-        else echo "reboot"
+if ping -c 1 -w 5 8.8.8.8 > '/dev/null' 2>&1
+then exit
+    elif ping -c 1 -w 10.188.0.241 > '/dev/null' 2>&1
+        then exit
+        else reboot
         
 fi
