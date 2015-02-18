d88fbc5e80d8e392d4931a727f36b21399a23504
stebifan <stefan@tro-mail.de>
2015-02-18 20:54:53 +0100

    Add DNS Server for Ping

--- netwatch/files/lib/tro/netwatch/netwatch.sh
+++ netwatch/files/lib/tro/netwatch/netwatch.sh
@@ -1,10 +1,10 @@
 if ping -c 1 -w 5 8.8.8.8 > '/dev/null' 2>&1
-		then exit
-    elif ping -c 1 -w 5 10.188.0.241 > '/dev/null' 2>&1
-        then exit
+	then exit
     elif ping6 -c 1 -w 5 2001:4860:4860::8888 > '/dev/null' 2>&1
     	then exit
     elif ping6 -c 1 -w 5 fda0:747e:ab29:7405::101 > '/dev/null' 2>&1
+    	then exit
+    elif ping6 -c 1 -w 5 fda0:747e:ab29:7405::100 > '/dev/null' 2>&1
     	then exit
         else reboot        
 fi
