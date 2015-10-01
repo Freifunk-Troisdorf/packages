8deedde198f47b74be3127b830770844c7b31db6
stebifan <stefan@tro-mail.de>
2015-10-01 16:47:24 +0200

    Updated IPs

--- netwatch/files/lib/tro/netwatch/netwatch.sh
+++ netwatch/files/lib/tro/netwatch/netwatch.sh
@@ -1,14 +1,14 @@
-if ping6 -c 1 -w 5 fda0:747e:ab29:7405::100 > '/dev/null' 2>&1
+if ping6 -c 5 -w 5 fda0:747e:ab29:7405::100 > '/dev/null' 2>&1
 	then exit 0
-    elif ping6 -c 1 -w 5 fda0:747e:ab29:7405::1 > '/dev/null' 2>&1
+    elif ping6 -c 5 -w 5 fda0:747e:ab29:7405:255::1 > '/dev/null' 2>&1
     	then exit 0
-    elif ping6 -c 1 -w 5 fda0:747e:ab29:7405::2 > '/dev/null' 2>&1
+    elif ping6 -c 5 -w 5 fda0:747e:ab29:7405:255::2 > '/dev/null' 2>&1
     	then exit 0
-    elif ping6 -c 1 -w 5 fda0:747e:ab29:7405::3 > '/dev/null' 2>&1
+    elif ping6 -c 5 -w 5 fda0:747e:ab29:7405:255::3 > '/dev/null' 2>&1
     	then exit 0
-    elif ping6 -c 1 -w 5 fda0:747e:ab29:7405::4 > '/dev/null' 2>&1
+    elif ping6 -c 5 -w 5 fda0:747e:ab29:7405:255::4 > '/dev/null' 2>&1
     	then exit 0
-    elif ping6 -c 1 -w 5 fda0:747e:ab29:7405::23 > '/dev/null' 2>&1
+    elif ping6 -c 5 -w 5 fda0:747e:ab29:7405::23 > '/dev/null' 2>&1
     	then exit 0
         else reboot        
 fi
