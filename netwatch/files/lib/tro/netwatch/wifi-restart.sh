1b8bc81bbbad1e765ea76d18d88a9489e54c02e8
Stefan Hoffmann <stefan@tro-mail.de>
2016-09-08 11:20:38 +0200

    Added wifi-reboot Script

---
+++ netwatch/files/lib/tro/netwatch/wifi-restart.sh
@@ -0,0 +1,16 @@
+#!/bin/ash
+# Run this script every 5 Minutes
+WIFIFAIL=/var/run/wifi_fail
+ROLE=mesh
+neighbours=`/usr/sbin/batctl o | /bin/grep mesh0 | /usr/bin/wc -l`
+getrole=`/sbin/uci get gluon-node-info.@system[0].role`
+
+if [ $getrole = $ROLE ]; then
+	if [ $neighbours -lt 1 ];
+		/bin/sleep 120
+		/sbin/wifi
+	else
+	exit 0
+else
+	exit 0
+fi
