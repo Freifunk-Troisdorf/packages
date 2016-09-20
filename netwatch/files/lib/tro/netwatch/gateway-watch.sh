79aeea916726acb8771fb97fbc173887e6db3f36
stebifan <stefan@tro-mail.de>
2016-09-20 09:39:53 +0200

    Update gateway-watch.sh

--- netwatch/files/lib/tro/netwatch/gateway-watch.sh
+++ netwatch/files/lib/tro/netwatch/gateway-watch.sh
@@ -1,12 +1,11 @@
 #!/bin/ash
 # Run this script every 5 Minutes
 ROLE=uplink
-gateways=`/usr/sbin/batctl gwl | /bin/grep "=>" | /usr/bin/wc -l`
 getrole=`/sbin/uci get gluon-node-info.@system[0].role`
 
 if [ $getrole = $ROLE ]; then
 	/bin/sleep 120
-	if [ $gateways -lt 1 ]; then
+	if [ `/usr/sbin/batctl gwl | /bin/grep "=>" | /usr/bin/wc -l` -lt 1 ]; then
 		/sbin/reboot
 	else
 		exit 0
@@ -14,13 +13,12 @@ if [ $getrole = $ROLE ]; then
 fi
 
 ROLE1=meshanduplink
-neighbours=`/usr/sbin/batctl o | /bin/grep mesh0 | /usr/bin/wc -l`
 
 if [ $getrole = $ROLE1 ]; then
 	/bin/sleep 120
-	if [ $neighbours -lt 1 ]; then
+	if [ `/usr/sbin/batctl o | /bin/grep mesh0 | /usr/bin/wc -l` -lt 1 ]; then
 		/sbin/wifi
-	elif [ $gateways -lt 1 ]; then
+	elif [ `/usr/sbin/batctl gwl | /bin/grep "=>" | /usr/bin/wc -l` -lt 1 ]; then
 		/sbin/reboot
 	fi
 else
