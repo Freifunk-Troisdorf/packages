da6afa0974f16bd0188070c26eae71d33daf2276
stebifan <stefan@tro-mail.de>
2016-09-20 09:40:36 +0200

    Update wifi-restart.sh

--- netwatch/files/lib/tro/netwatch/wifi-restart.sh
+++ netwatch/files/lib/tro/netwatch/wifi-restart.sh
@@ -1,12 +1,11 @@
 #!/bin/ash
 # Run this script every 5 Minutes
 ROLE=mesh
-neighbours=`/usr/sbin/batctl o | /bin/grep mesh0 | /usr/bin/wc -l`
 getrole=`/sbin/uci get gluon-node-info.@system[0].role`
 
 if [ $getrole = $ROLE ]; then
 		/bin/sleep 60
-		if [ $neighbours -lt 1 ]; then
+		if [ `/usr/sbin/batctl o | /bin/grep mesh0 | /usr/bin/wc -l` -lt 1 ]; then
 		/sbin/wifi
 	fi
 else
