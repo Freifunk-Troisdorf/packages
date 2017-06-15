027d9e6e2a95abcb50847c3554aae93245adf4bc
stebifan <stefan@tro-mail.de>
2017-06-15 19:38:30 +0200

    Create gateway-watch.sh

--- netwatch/files/lib/tro/netwatch/gateway-watch.sh
+++ netwatch/files/lib/tro/netwatch/gateway-watch.sh
@@ -5,7 +5,7 @@ getrole=`/sbin/uci get gluon-node-info.@system[0].role`
 
 if [ $getrole = $ROLE ]; then
 	/bin/sleep 120
-	if [ `/usr/sbin/batctl gwl | /bin/grep "=>" | /usr/bin/wc -l` -lt 1 ]; then
+	if [ `/usr/sbin/batctl gwl | /bin/grep "*" | /usr/bin/wc -l` -lt 1 ]; then
 		/sbin/reboot
 	else
 		exit 0
@@ -18,7 +18,7 @@ if [ $getrole = $ROLE1 ]; then
 	/bin/sleep 120
 	if [ `/usr/sbin/batctl o | /bin/grep mesh0 | /usr/bin/wc -l` -lt 1 ]; then
 		/sbin/wifi
-	elif [ `/usr/sbin/batctl gwl | /bin/grep "=>" | /usr/bin/wc -l` -lt 1 ]; then
+	elif [ `/usr/sbin/batctl gwl | /bin/grep "*" | /usr/bin/wc -l` -lt 1 ]; then
 		/sbin/reboot
 	fi
 else
